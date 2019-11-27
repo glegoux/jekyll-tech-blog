9798976d1a96eb14e22910aff05e310f27d967a1
glegoux <gilles.legoux@gmail.com>
2019-11-28 00:40:59 +0100

    Migrate first article

--- website/js/main.js
+++ website/js/main.js
@@ -33,49 +33,5 @@ jQuery(document).ready(function () {
     }
   });
 
-  // Multi-lang
-
-  let originalLanguage = "english";
-  let originalLangHeader = $("#original-lang");
-  let langs = $(".lang");
-
-  function changeLanguage(language) {
-    if (originalLanguage === language) {
-      originalLangHeader.hide();
-    } else {
-      originalLangHeader.show();
-    }
-    langs.each(function () {
-      if (language === $(this).data("lang")) {
-        $(this).find("span").addClass("focus");
-      } else {
-        $(this).find("span").removeClass("focus");
-      }
-    });
-    // Called twice
-    googleTranslate(language);
-    googleTranslate(language);
-    window.localStorage.setItem('lang', language);
-  }
-
-  // init translation
-  let currentLanguage = window.localStorage.getItem('lang');
-  if (currentLanguage === null) {
-    let browserLanguage = navigator.language.substring(0, 2);
-    if (browserLanguage === "fr") {
-      changeLanguage("french");
-    } else {
-      changeLanguage(originalLanguage);
-    }
-  } else {
-    changeLanguage(currentLanguage);
-  }
-
-  // update translation
-  langs.click(function () {
-    let language = $(this).data("lang");
-    changeLanguage(language);
-  });
-
 });
 
