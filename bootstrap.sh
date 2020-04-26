8fda40555136d07c4d3608909da4c6da105cc32a
Gilles Legoux <g.legoux@criteo.com>
2020-04-26 13:23:04 +0200

    Fix bootstrap.sh for rvm usage

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,13 @@
+# bash script
 # usage: source boostrap.sh
 
-exec bash --login --init-file <(echo 'rvm use "$(cat .ruby-version)"')
+ruby_project_version="$(cat .ruby-version | tr -d '\n')"
+ruby_local_version="$(which ruby &> /dev/null && ruby --version | cut -f2 -d' ' | cut -f1 -d'p')"
+
+if [[ "${ruby_local_version}" != "${ruby_project_version}" ]]; then
+  source ~/.profile
+  rvm use "${ruby_project_version}" \
+    || >&2 echo "ERROR: Please install rvm, see https://rvm.io/, then ruby ${ruby_project_version} with that"
+else
+  >&2 echo "WARNING: You use already ruby ${ruby_project_version}"
+fi
