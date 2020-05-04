e1173fc27750ba8d553f0372d1f0ee090d8457ce
Gilles Legoux <g.legoux@criteo.com>
2020-05-05 00:27:47 +0200

    Add quote for bash script

--- bin/integrity-assets.sh
+++ bin/integrity-assets.sh
@@ -5,14 +5,14 @@ set -e
 url="${1}"
 encryption="${2:-sha256}"
 
-_type=${url##*.}
+_type="${url##*.}"
 if [[ "${_type}" != 'css' ]] && [[ "${_type}" != 'js' ]]; then
   >&2 echo "ERROR: only urls to css and js content are authorized!"
   exit 1
 fi
 
-content=$(curl -sS --fail "${url}")
-integrity=$(echo -n "${content}" | openssl dgst -${encryption} -binary | openssl base64 -A)
+content="$(curl -sS --fail "${url}")"
+integrity="$(echo -n "${content}" | openssl dgst "-${encryption}" -binary | openssl base64 -A)"
 
 if [[ "${_type}" = 'css' ]] ; then
   echo "<link href=\"${url}\" rel=\"stylesheet\" type=\"text/css\" integrity=\"${encryption}-${integrity}\" crossorigin=\"anonymous\">"
