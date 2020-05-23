152383a3e5248afd30cfd262f4990265df772927
Gilles Legoux <gilles.legoux@gmail.com>
2020-05-23 03:03:01 +0200

    Fix permissions and exit status if failure for tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,10 +4,21 @@ set -e
 
 cd "$(git rev-parse --show-toplevel)" || exit 1
 
+run_test() {
+  local message="$1"
+  local execute="$2"
+  echo -n "- ${message}..."
+  if eval "${execute}"; then
+    echo "OK"
+  else
+    echo "Failed"
+    exit 1
+  fi
+}
+
 project_dir="dest/"
 
-echo "** Check:"
-echo -n "- CSS files..."
-./test/checker/checker_css.sh "${project_dir}" && echo "OK" || echo "Failed"
-echo -n "- JSON files..."
-./test/checker/checker_json.sh "${project_dir}" && echo "OK" || echo "Failed"
+echo "Check:"
+run_test  "CSS files" "./test/checker/checker_css.sh \"${project_dir}\""
+run_test  "JSON files" "./test/checker/checker_json.sh \"${project_dir}\""
+
