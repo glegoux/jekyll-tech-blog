bcc06f1d4a37139b97b9d7daefffbf15e44fc6ba
Gilles Legoux <gilles.legoux@gmail.com>
2023-11-28 15:24:42 +0100

    Update inspect-new-gem-versions.sh

--- test/inspect/inspect-new-gem-versions.sh
+++ test/inspect/inspect-new-gem-versions.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
-# See the latest available version of each gem according the version constraints of the project
+# See the latest available version of each gem according to the version constraints of the project
+# (only for the version not already used)
 
 cd "$(git rev-parse --show-toplevel)" || exit 1
 
