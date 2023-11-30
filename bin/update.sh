2aae8d16e04d479e855e0a52e38b821c0be9fb0a
Gilles Legoux <gilles.legoux@gmail.com>
2023-11-30 21:30:43 +0100

    Improve bin/update.sh script

--- bin/update.sh
+++ bin/update.sh
@@ -4,9 +4,31 @@
 #
 # usage: ./update.sh
 
+cd "$(git rev-parse --show-toplevel)" || exit 1
+
+changes="$(git status -s | cut -c4-)"
+[[ -z $(git status -s | cut -c4-) ]] || \
+  >&2 echo "ERROR: cannot update due to local changes, stash or commit them!" \
+  && exit 1
+
 git fetch origin main:main
 # without committing it automatically if no conflict exists in the merge results
 git merge --no-commit --no-ff main
-# git reset -- /path/to/folder # revert updates from path
+# ignore these changes
+core_project_resources="$(find src/_core/* -maxdepth 1 -name 'project' -type d)"
+project_resources="$(find src/* -maxdepth 0 ! \( -name "_core" -type d \))"
+other_resources=".github/ resources/ README.md"
+for change in ${changes}; do
+  for resource in ${core_project_resources} ${project_resources} ${other_resources}; do
+    if [[ $resource =~ $change ]]; then
+        echo "INFO: Ignore this change: ${change}"
+        git add "${change}" &> /dev/null && \
+        git reset "${change}" &> /dev/null && \
+        git checkout "${change}" &> /dev/null
+        continue
+    fi
+  done
+done
 
-echo "Use 'git commit' to validate the changes of the update!"
+[[ -n $(git status -s | cut -c4-) ]]  && \
+  echo "Use 'git commit' to validate the changes of the update!"
